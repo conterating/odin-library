29687e169b5f3ecae467e25e91e23291c941ba81
Aashay Gupte <151937841+conterating@users.noreply.github.com>
2024-07-28 22:19:31 -0400

    Allowed for book instance to be pushed to library array

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const myLibrary = [];
+
+function Book(title, author, pages, readStatus) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.readStatus = readStatus;
+  this.info = function () {
+    return `${this.title} by ${this.author}, ${this.pages}, ${this.readStatus}`;
+  };
+}
+
+function addBookToLibrary() {
+  let userBook = new Book();
+  const bookInfo = prompt(
+    "Enter book information: (title), (author), (pages), (readStatus)"
+  );
+
+  [userBook.title, userBook.author, userBook.pages, userBook.readStatus] =
+    bookInfo.split(", ");
+
+  myLibrary.push(userBook);
+}
+
+addBookToLibrary();
+
+console.log(myLibrary);
